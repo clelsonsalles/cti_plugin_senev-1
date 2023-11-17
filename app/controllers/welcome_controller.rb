886d37d53ca7600c33dd36003e81e542e3ee3b6e
Bruna Guedes <bruna.guedes02@gmail.com>
2023-11-17 14:48:03 -0300

    add projetos na view welcome

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,18 @@
+class WelcomeController < ApplicationController
+  self.main_menu = false
+
+  skip_before_action :check_if_login_required, only: [:robots]
+
+  helper :custom_fields
+
+  def index
+    @news = News.latest User.current
+    @projects = Project.all
+  end
+
+  def robots
+    @projects = Project.visible(User.anonymous) unless Setting.login_required?
+    render :layout => false, :content_type => 'text/plain'
+  end
+
+end
