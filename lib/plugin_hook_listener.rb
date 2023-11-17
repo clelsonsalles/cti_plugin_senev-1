098c70ce01573217cf625776c6b02fa55e3968e7
Bruna Guedes <bruna.guedes02@gmail.com>
2023-11-17 16:02:41 -0300

    footer

--- lib/plugin_hook_listener.rb
+++ lib/plugin_hook_listener.rb
@@ -1,3 +1,4 @@
 class PluginHookListener < Redmine::Hook::ViewListener
   render_on :view_layouts_base_html_head, partial: "plugin_agenda_mulheres/base_html"
+  render_on :view_layouts_base_body_bottom, partial: "plugin_agenda_mulheres/footer"
 end
